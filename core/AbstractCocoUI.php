c0686c9eb89a07093e0189e09aef5a96ebebbb06
Yannick Lohse <yannick.lohse@caldera.com>
2014-02-11 12:37:17 +0100

    Automatic nonce validation

--- core/AbstractCocoUI.php
+++ core/AbstractCocoUI.php
@@ -10,16 +10,25 @@ abstract class AbstractCocoUI{
 		$this->name = $name;
 	}
 	
+	public function getValue(){
+		return $this->value;
+	}
+	
+	public function preventFilters(){
+		$this->value = false;
+	}
+	
 	public function filter($filter, $params=array()){
 		//on first run, load the posted value
 		if ($this->value === null) $this->value = CocoStore::request($this->name);
 		
 		//run through the filters
-		if ($this->value === false) return;
-		else{
+		if ($this->value !== false){//prevents the remaining filters to run, either if no value was found or purposely by a filter
 			if (class_exists($filter)){
 				$this->value = $filter::apply($this->value, $params);
 			}
 		}
+		
+		return $this;
 	}
 }
--- core/Cocorico.php
+++ core/Cocorico.php
@@ -2,13 +2,18 @@
 class Cocorico{
 	
 	protected $forms = array();
+	protected $validated = true;//default to true so that the nonce filter runs
 	
 	public function __construct(){
+		$nonce_action = 'cocorico_nonce_validation';
+		$nonce = $this->field('CocoNonceUI', 'coco_nonce', array('action'=>$nonce_action))->filter('CocoNonceFilter', array('action'=>$nonce_action));
+		$this->validated = (bool)$nonce->getValue();
 	}
 	
 	public function field($form, $name, $params=array()){
 		if (class_exists($form)){
 			$form = new $form($name);
+			if (!$this->validated) $form->preventFilters();
 			array_push($this->forms, array($form, $params));
 			return $form;
 		}
