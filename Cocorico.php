1e96139825feafc214847c82ba0125332340e95e
Yannick Lohse <yannick.lohse@caldera.com>
2014-02-11 11:50:52 +0100

    Added radio ui

--- Cocorico.php
+++ Cocorico.php
@@ -6,5 +6,6 @@ require_once 'core/AbstractCocoUI.php';
 require_once 'core/AbstractCocoFilter.php';
 
 require_once 'uis/CocoTextUI.php';
+require_once 'uis/CocoRadioUI.php';
 
 require_once 'filters/CocoSaveFilter.php';
---
+++ uis/CocoRadioUI.php
@@ -0,0 +1,21 @@
+<?php
+class CocoRadioUI extends AbstractCocoUI{
+	
+	public function render($params){
+		$output = '';
+		$options = $params['options'];
+		$selected = get_option($this->name);
+		
+		foreach ($options as $label=>$value){
+			$output .= '
+			<label>
+				<input type="radio" name="'.$this->name.'" value="'.$value.'" '.(($selected == $value) ? 'checked="checked"' : '').' />
+				'.$label.'
+			</label>
+			';
+		}
+		
+		return $output;
+	}
+	
+}
