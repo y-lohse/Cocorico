dbef791039dd263d7e9ae1484e0b6c2c7034f5fa
Yannick Lohse <glorf.travian@gmail.com>
2014-02-15 19:22:52 +0100

    Security and options

--- plugins/CocoricoUIs.php
+++ plugins/CocoricoUIs.php
@@ -7,28 +7,58 @@ CocoDictionary::register('ui', 'nonce', 'cocoricoNonceUI');
 
 //submit button
 function cocoricoSubmitUI($name, $options=array()){
-	$output = '<input type="submit" name="'.$name.'"';
-	if (isset($options['value'])) $output .= ' value="'.$options['value'].'"';
+	$options = array_merge(array(
+		'class'=>array('button', 'button-primary')
+	), $options);
+	
+	$attrs = array(
+		'type'=>'submit',
+		'name'=>$name,
+	);
+	$attrs['class'] = (is_array($options['class'])) ? implode($options['class'], ' ') : $options['class'];
+	if (isset($options['value'])) $attrs['value'] = $options['value'];
+	
+	$output = '<input';
+	foreach ($attrs as $name=>$value){
+		$output .= ' '.$name.'="'.esc_attr($value).'"';
+	}
 	$output .= ' />';
+	
 	return $output;
 }
 CocoDictionary::register('ui', 'submit', 'cocoricoSubmitUI');
 
 //label
 function cocoricoLabelUI($label, $for){
-	$output = '<label for="'.$for.'">'.$label.'</label>';
+	$output = '<label for="'.esc_attr($for).'">'.$label.'</label>';
 	return $output;
 }
 CocoDictionary::register('ui', 'label', 'cocoricoLabelUI');
 
 //text input
 function cocoricoTextUI($name, $options=array()){
+	$options = array_merge(array(
+	), $options);
+	
 	//saved is the key to a wordpress option containing the value
 	//value is the direct value to output
 	$option_key = (isset($options['saved'])) ? $options['saved'] : $name;
 	$value = (isset($options['value'])) ? $options['value'] : get_option($option_key);
 	
-	$output = '<input type="text" name="'.$name.'" id="'.$name.'" value="'.$value.'" />';
+	$attrs = array(
+		'type'=>'text',
+		'name'=>$name,
+		'id'=>$name,
+		'value'=>$value
+	);
+	$attrs['class'] = (is_array($options['class'])) ? implode($options['class'], ' ') : $options['class'];
+	
+	$output = '<input';
+	foreach ($attrs as $name=>$value){
+		$output .= ' '.$name.'="'.esc_attr($value).'"';
+	}
+	$output .= ' />';
+	
 	return $output;
 }
 CocoDictionary::register('ui', 'text', 'cocoricoTextUI');
