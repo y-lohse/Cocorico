a645c30a90d3b358a2dc3e10d007bf5ed62c4f78
Yannick Lohse <glorf.travian@gmail.com>
2014-02-15 17:49:44 +0100

    Added shorthands for form tables #6

--- plugins/CocoricoShorthands.php
+++ plugins/CocoricoShorthands.php
@@ -1,4 +1,17 @@
 <?php
+//starts a table form wrapper
+function cocoricoFormTableStartShorthand($cocorico){
+	$cocorico->startWrapper('form-table');
+}
+CocoDictionary::register('shorthand', 'startForm', 'cocoricoFormTableStartShorthand');
+
+//ends a table form wrapper
+function cocoricoFormTableEndShorthand($cocorico){
+	$cocorico->endWrapper('form-table');
+}
+CocoDictionary::register('shorthand', 'endForm', 'cocoricoFormTableEndShorthand');
+
+//input field in a table
 function cocoricoSettingShorthand($cocorico, $params){
 	$cocorico->startWrapper('tr');
 	
