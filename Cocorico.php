315b14f7e515a12bb88843756f9057e71052f75c
Yannick Lohse <glorf.travian@gmail.com>
2014-06-15 16:20:54 +0200

    Load local extensions

--- Cocorico.php
+++ Cocorico.php
@@ -2,7 +2,7 @@
 
 if (!defined('COCORICO_PATH')){
 	define('COCORICO_PATH', dirname(__FILE__));
-
+	
 	//Cocorico is supposed to be dropped in a plugin or a theme-get the url either way
 	if (strpos(COCORICO_PATH, get_theme_root()) >= 0){
 		$rootlessPath = substr(COCORICO_PATH, strlen(get_theme_root()));
@@ -12,20 +12,14 @@ if (!defined('COCORICO_PATH')){
 		$url = plugin_dir_url(__FILE__);
 		define('COCORICO_URI', substr($url, 0, strlen($url)-1));
 	}
-
-	//autoload everything
+	
+	//autoload cocorico core
 	foreach (array('core', 'plugins') as $dir){
 		foreach (glob(COCORICO_PATH.'/'.$dir.'/*.php') as $file){
 			require_once $file;
 		}
 	}
-
-	foreach (glob(COCORICO_PATH.'/extensions/*', GLOB_ONLYDIR) as $extension){
-		foreach (glob($extension.'/*.php') as $file){
-			require_once $file;
-		}
-	}
-
+	
 	if (!function_exists('cocorico_enqueue')){
 		function cocorico_enqueue(){
 			wp_register_script('cocorico', COCORICO_URI.'/frontend/cocorico.js', array('jquery'), '1', true);
@@ -38,4 +32,13 @@ if (!defined('COCORICO_PATH')){
 		}
 	}
 	add_action('admin_enqueue_scripts', 'cocorico_enqueue');
+}
+
+//load local extensions
+$localCocoPath = dirname(__FILE__);
+
+foreach (glob($localCocoPath.'/extensions/*', GLOB_ONLYDIR) as $extension){
+	foreach (glob($extension.'/*.php') as $file){
+		require_once $file;
+	}
 }
