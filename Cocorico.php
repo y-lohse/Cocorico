56a95895cc8e6de007b071e7f594bb717d88e7b5
Yannick Lohse <glorf.travian@gmail.com>
2014-06-25 21:25:48 +0200

    Removed usage of COCORICO_URI

--- Cocorico.php
+++ Cocorico.php
@@ -3,16 +3,6 @@
 if (!defined('COCORICO_PATH')){
 	define('COCORICO_PATH', dirname(__FILE__));
 	
-	//Cocorico is supposed to be dropped in a plugin or a theme-get the url either way
-	if (strpos(COCORICO_PATH, get_theme_root())){
-		$rootlessPath = substr(COCORICO_PATH, strlen(get_theme_root()));
-		define('COCORICO_URI', get_theme_root_uri().str_replace('\\', '/', $rootlessPath));
-	}
-	else{
-		$url = plugin_dir_url(__FILE__);
-		define('COCORICO_URI', substr($url, 0, strlen($url)-1));
-	}
-	
 	//autoload cocorico core
 	foreach (array('core', 'plugins') as $dir){
 		foreach (glob(COCORICO_PATH.'/'.$dir.'/*.php') as $file){
@@ -22,12 +12,24 @@ if (!defined('COCORICO_PATH')){
 	
 	if (!function_exists('cocorico_enqueue')){
 		function cocorico_enqueue(){
-			wp_register_script('cocorico', COCORICO_URI.'/frontend/cocorico.js', array('jquery'), '1', true);
+			//Cocorico is supposed to be dropped in a plugin or a theme-get the url either way
+			if (strpos(COCORICO_PATH, get_theme_root()) === 0){
+				$rootlessPath = substr(COCORICO_PATH, strlen(get_theme_root()));
+				$coco_path = get_theme_root_uri().str_replace('\\', '/', $rootlessPath);
+			}
+			else{
+				$url = plugin_dir_url(__FILE__);
+				$coco_path = substr($url, 0, strlen($url)-1);
+			}
+			
+			wp_register_script('cocorico', $coco_path.'/frontend/cocorico.js', array('jquery'), '1', true);
 			wp_enqueue_script('cocorico');
 
 			wp_enqueue_style('wp-color-picker');
 			wp_enqueue_script('wp-color-picker');
-
+			
+			do_action('cocorico_enqueue', $coco_path);
+			
 			wp_enqueue_media();
 		}
 	}
