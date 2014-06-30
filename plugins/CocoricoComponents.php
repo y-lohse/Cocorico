413bfea5758efd8f031f16b4445164642106a457
Yannick Lohse <contact@yannick-lohse.fr>
2014-06-30 12:26:36 +0200

    Remove select before & after

--- plugins/CocoricoComponents.php
+++ plugins/CocoricoComponents.php
@@ -210,23 +210,17 @@ CocoDictionary::register(CocoDictionary::COMPONENT, 'radio', 'cocoricoRadioCompo
 
 //select 
 function cocoricoSelectComponent($component, $selects, $options=array()){
-	$options = array_merge(array(
-		'before'=>'',
-		'after'=>''
-	), $options);
-		
 	$output = '<select name="'.$component->getName().'">';
 	$selected = (!$component->getValue() && isset($options['default'])) ? $options['default'] : $component->getValue();
 
 	foreach ($selects as $value=>$label){
-		$output .= $options['before'];
 		$output .= '
 			<option type="radio" value="'.$value.'" '.(($selected == $value) ? 'selected' : '').'>
 			'.$label.'</option>
 		';
-		$output .= $options['after'];
 	}
-
+	$output .= '</select>';
+	
 	return $output;
 }
 CocoDictionary::register(CocoDictionary::COMPONENT, 'select', 'cocoricoSelectComponent');
